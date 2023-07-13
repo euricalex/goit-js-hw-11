ce60a2c88bac318e8fa938872ce05fb3dd327a93
Alexander <122707746+euricalex@users.noreply.github.com>
2023-07-13 17:35:11 +0300

    update

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import Notiflix from 'notiflix';
+// Описаний в документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+  
+
+const searchForm = document.querySelector("#search-form");
+const inputText = document.querySelector("input[type='text']");
+const btnSubmit = document.querySelector("button[type='submit']");
+const galleryList = document.querySelector(".gallery p")
+
+
+searchForm.addEventListener('submit', onSearchFormSubmit);
+const MY_KEY = '38235437-3b164ab9bac08b48d72521750';
+
+const BASE_URL = `https://pixabay.com/api/?key=${MY_KEY}&q=`;
+
+function onSearchFormSubmit(event) {
+  event.preventDefault();
+  const input = inputText.value;
+  if (input) {
+    const url = `${BASE_URL}${encodeURIComponent(input)}&image_type=photo&orientation=horizontal&safesearch=true`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+       if(data.hits.length === 0) {
+        Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.")
+       } else {
+        console.log(data);
+        renderGallery(data.hits);
+        initializeLightBox();
+       }
+      })
+      .catch(error => {
+        console.log('Error:', error);
+        Notiflix.Notify.failure("An error occurred. Please try again later.");
+      });
+  } 
+}
+function renderGallery(images) {
+    galleryList.innerHTML = '';
+    images.forEach(image => {
+        const galleryItem = document.createElement("div");
+        galleryItem.classList.add("photo-card")
+        galleryItem.innerHTML = `
+        <div class="photo-card">
+        <a href="${image.largeImageURL}" data-caption="${image.tags}">
+        <img src="${image.webformatURL}" alt="${image.tags}" loading="lazy" />
+      </a>
+        <div class="info">
+          <p class="info-item">
+            <b>Likes</b>${image.likes}</p>
+          <p class="info-item">
+            <b>Views</b>${image.views}</p>
+          <p class="info-item">
+            <b>Comments</b>${image.comments}</p>
+          <p class="info-item">
+            <b>Downloads</b>${image.downloads}</p>
+        </div>
+        </div>`;
+        galleryList.appendChild(galleryItem);
+    });  
+}
+function initializeLightBox() {
+    const lightbox = new SimpleLightbox(".gallery a", {
+      captionDelay: 250,
+      captionsData: "alt",
+    });
+  }
+
+
