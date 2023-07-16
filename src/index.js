3fd4b55bfbe55d37133115ea49e3ac1064826646
Alexander <122707746+euricalex@users.noreply.github.com>
2023-07-16 22:46:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,82 +1,76 @@
 import Notiflix from 'notiflix';
-import axios from "axios";
-
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-
 
 const searchForm = document.querySelector('#search-form');
 const inputText = document.querySelector("input[type='text']");
 const btnSubmit = document.querySelector("button[type='submit']");
 const galleryList = document.querySelector('.gallery');
 const loadMoreBtn = document.querySelector('.load-more');
-axios.defaults.headers.common["x-api-key"] = '38235437-3b164ab9bac08b48d72521750';
 
 searchForm.addEventListener('submit', onSubmit);
 loadMoreBtn.addEventListener('click', loadMoreImages);
-// hideLoadMoreButton();
 
 let currentPage = 1;
 let totalHits = 0;
 const resultsPerPage = 40;
+let initialQuery = '';
 
 const MY_KEY = '38235437-3b164ab9bac08b48d72521750';
 const BASE_URL = `https://pixabay.com/api/?key=${MY_KEY}&q=`;
 
-async function onSubmit(event) {
+function onSubmit(event) {
   event.preventDefault();
-  const input = inputText.value;
+  const input = inputText.value.trim();
   if (input) {
     currentPage = 1;
-    await searchImages(input);
+    initialQuery = input; // Зберігаємо початковий запит
+    searchImages(input);
+  } else {
+    Notiflix.Notify.warning("Please enter a search query.");
   }
-
 }
 
 async function loadMoreImages() {
-  currentPage++;
-  await searchImages(inputText.value);
- 
+  if (inputText.value.trim() === initialQuery) {
+    currentPage++;
+    await searchImages(inputText.value);
+  } else {
+    Notiflix.Notify.warning("Cannot load more images. Please continue with the current search query.");
+  }
 }
-
 
 async function searchImages(query) {
   const url = `${BASE_URL}&q=${encodeURIComponent(query)}&image_type=photo&orientation=horizontal&safesearch=true&page=${currentPage}&per_page=${resultsPerPage}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
-   
 
     if (data.hits.length === 0) {
       Notiflix.Notify.failure("Sorry, there are no images matching your search query. Please try again.");
       hideLoadMoreButton();
-   
       return;
     }
 
     if (currentPage === 1) {
       galleryList.innerHTML = '';
-      
     }
 
     totalHits = data.totalHits;
     renderGallery(data.hits);
 
     if (currentPage * resultsPerPage >= totalHits) {
-  
       Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
       hideLoadMoreButton();
     } else {
       showLoadMoreButton();
     }
-
     const { height: cardHeight } = galleryList.firstElementChild.getBoundingClientRect();
 
     window.scrollTo({
       top: window.pageYOffset + cardHeight * 2,
       behavior: "smooth",
     });
-
     const totalHitsMessage = `Hooray! We found ${totalHits} images.`;
     Notiflix.Notify.success(totalHitsMessage);
 
@@ -105,23 +99,22 @@ function renderGallery(images) {
         </a>
         <div class="info">
           <p class="info-item">
-            <b>Likes</b> ${image.likes}
+            <b>Likes:</b> ${image.likes}
           </p>
           <p class="info-item">
-            <b>Views</b> ${image.views}
+            <b>Views:</b> ${image.views}
           </p>
           <p class="info-item">
-            <b>Comments</b> ${image.comments}
+            <b>Comments:</b> ${image.comments}
           </p>
           <p class="info-item">
-            <b>Downloads</b> ${image.downloads}
+            <b>Downloads:</b> ${image.downloads}
           </p>
         </div>
       </div>`;
     galleryList.appendChild(galleryItem);
   });
 }
-
 
 function initializeLightbox() {
   const lightbox = new SimpleLightbox('.gallery a', {
@@ -137,6 +130,7 @@ function hideLoadMoreButton() {
 function showLoadMoreButton() {
   loadMoreBtn.style.display = 'block';
 }
-function refreshLightBox () {
+
+function refreshLightBox() {
   SimpleLightbox.refresh();
 }
